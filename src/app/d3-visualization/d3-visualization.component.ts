1d199d1a7105f9bdb1dfb45e5d9ef48296d3f916
Anthony Rozario <98898217+AnthonyRCoder@users.noreply.github.com>
2024-06-06 15:34:52 +0100

    install D3 and generate component d3-visualization

---
+++ src/app/d3-visualization/d3-visualization.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d3-visualization',
+  standalone: true,
+  imports: [],
+  templateUrl: './d3-visualization.component.html',
+  styleUrl: './d3-visualization.component.scss'
+})
+export class D3VisualizationComponent {
+
+}
