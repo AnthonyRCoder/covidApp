2390663deb35ecbeb2c088ff54cc37ca9028f891
Anthony Rozario <98898217+AnthonyRCoder@users.noreply.github.com>
2024-06-10 15:05:32 +0100

    add chart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { D3VisualizationComponent } from './d3-visualization/d3-visualization.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: D3VisualizationComponent }
+];
--- src/app/d3-visualization/d3-visualization.component.ts
+++ src/app/d3-visualization/d3-visualization.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-d3-visualization',
@@ -7,6 +8,36 @@ import { Component } from '@angular/core';
   templateUrl: './d3-visualization.component.html',
   styleUrl: './d3-visualization.component.scss'
 })
-export class D3VisualizationComponent {
+export class D3VisualizationComponent implements OnInit {
 
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  private createChart(): void {
+    const data = [30, 86, 168, 281, 303, 365];
+
+    const svg = d3.select(this.el.nativeElement).select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        barWidth = width / data.length;
+
+    const bar = svg.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", (d, i) => `translate(${i * barWidth}, 0)`);
+
+    bar.append("rect")
+        .attr("y", d => height - d)
+        .attr("height", d => d)
+        .attr("width", barWidth - 1);
+
+    bar.append("text")
+        .attr("x", barWidth / 2)
+        .attr("y", d => height - d - 5)
+        .attr("dy", ".75em")
+        .text(d => d);
+  }
 }
